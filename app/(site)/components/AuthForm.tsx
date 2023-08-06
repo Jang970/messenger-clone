014578c2fc0ab917f34a3877a2a346c6a3574492
jang970 <jang970@aucklanduni.ac.nz>
2023-08-06 19:09:48 +1200

    started creating Authentication page UI, created form component

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import Input from "@/app/components/inputs/Input";
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+type Variant = "LOGIN" | "REGISTER";
+
+const AuthForm = () => {
+  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const [isLoading, setIsLoading] = useState(false);
+
+  // useCallback to memoize function
+  const toggleVariant = useCallback(() => {
+    if (variant === "LOGIN") {
+      setVariant("REGISTER");
+    } else {
+      setVariant("LOGIN");
+    }
+  }, [variant]);
+
+  // react hook form submit function
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    if (variant === "REGISTER") {
+      // axios register
+    }
+
+    if (variant === "LOGIN") {
+      // NextAuth Signin
+    }
+  };
+
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+
+    // NextAuth social Sign In
+  };
+
+  return (
+    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          <Input id="email" label="Email" register={register} errors={errors} />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AuthForm;
