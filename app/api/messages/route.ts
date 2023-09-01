ad1f8dfeb32d3dc610333277b81439af94d36001
jang970 <jang970@aucklanduni.ac.nz>
2023-09-01 19:30:31 +1200

    created message creation and message & image uploads

---
+++ app/api/messages/route.ts
@@ -0,0 +1,69 @@
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import { NextResponse } from "next/server";
+import prisma from "@/app/libs/prismadb";
+
+export async function POST(request: Request) {
+  try {
+    const currUser = await getCurrentUser();
+    const body = await request.json();
+
+    const { message, image, conversationId } = body;
+
+    if (!currUser?.id || !currUser?.email) {
+      return new NextResponse("unauthorized", { status: 401 });
+    }
+
+    const newMessage = await prisma.message.create({
+      data: {
+        body: message,
+        image: image,
+        conversation: {
+          connect: {
+            id: conversationId,
+          },
+        },
+        sender: {
+          connect: {
+            id: currUser.id,
+          },
+        },
+        seen: {
+          connect: {
+            id: currUser.id,
+          },
+        },
+      },
+      include: {
+        seen: true,
+        sender: true,
+      },
+    });
+
+    const updateConversation = await prisma.conversation.update({
+      where: {
+        id: conversationId,
+      },
+      data: {
+        lastMessageAt: new Date(),
+        messages: {
+          connect: {
+            id: newMessage.id,
+          },
+        },
+      },
+      include: {
+        users: true,
+        messages: {
+          include: {
+            seen: true,
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(newMessage);
+  } catch (error: any) {
+    console.log(error, "ERROR_MESSAGES");
+    return new NextResponse("internal error", { status: 500 });
+  }
+}
